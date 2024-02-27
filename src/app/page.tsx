8f6bd191f468a165b183b7740c7ca6ce4f3caa24
lcsleite <lucas.leite@sensedia.com>
2024-02-27 18:41:33 -0300

    navbar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 import Image from "next/image";
 import React from "react";
 import ConsentForm from "../components/ConsentForm/ConsentForm";
+import NavBar from "@/components/NavBar/NavBar";
 
 export default function Home() {
   return (
